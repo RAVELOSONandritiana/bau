d9b71d20a5111c106e7e5f793cc3e7ad2ff4e320
RAVELOSONandritiana <hgbmichel@gmail.com>
2026-03-12 13:26:45 +0300

    q success

--- src/app/features/collection-listing/collection-listing.ts
+++ src/app/features/collection-listing/collection-listing.ts
@@ -90,8 +90,8 @@ import { ScrollAnimationDirective } from '../../directives/scroll-animation.dire
                     </svg>
                   </span>
                   
-                  <div class="w-10 h-10 rounded-full border border-gray-100 flex items-center justify-center bg-gray-50 group-hover:bg-green-500 group-hover:text-white group-hover:border-green-500 transition-all duration-300">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <div class="w-10 h-10 rounded-full border border-gray-100 flex items-center justify-center bg-white transition-all duration-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
                     </svg>
                   </div>
